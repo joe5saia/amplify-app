233707424cd6e8955c10a4c7bb73604aecac3ec8
Joe Saia <joe5saia@gmail.com>
2023-09-30 12:14:24 -0400

    new content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Your Name</h1>
+        <img src="your-photo-url.jpg" className="App-logo" alt="Your Portrait" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          PhD in Economics from Columbia University | Machine Learning Enthusiast
+        </p>
+        <p>
+          Experienced in leveraging machine learning to enhance econometric methods and analyze macroeconomic effects of monetary policy. Former fintech startup professional specializing in standardizing workforce data for insightful analytics. Currently leading engineering and business intelligence teams at Perry Health, focusing on evaluating clinical effectiveness.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/joe-saia/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Connect on LinkedIn
         </a>
       </header>
     </div>
